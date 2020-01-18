620141bb3a12d11b989646248c57a6ca19ea8b42
quietpochatok <bosroin@gmail.com>
2020-01-19 04:27:40 +0500

    add Clients(table) migration

--- db/migrate/20200118161335_create_clients.rb
+++ db/migrate/20200118161335_create_clients.rb
@@ -1,14 +1,14 @@
 class CreateClients < ActiveRecord::Migration[6.0]
-  def changet.text
-  	
+  def change
   	create_table :client do |t|
+      #будут созданы следующие столбы с типом text
   		t.text :name
   		t.text :phone
   		t.text :datestamp
   		t.text :barber
   		t.text :color
-
-		t.timestamps  		
+      # дата создания сущностей и обновления
+		  t.timestamps  		
   	end
   end
 end
