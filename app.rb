5a517f748c5ec181cc10da0e0338b5a64cf2f6cf
quietpochatok <bosroin@gmail.com>
2020-01-25 01:34:49 +0500

    add minor & ~g refactoring

--- app.rb
+++ app.rb
@@ -12,56 +12,25 @@ class Barber < ActiveRecord::Base
 end
 
 
-
-# def is_barber_exists? db, nameBarber
-# 	db.execute('select * from Barbers where name=?', [name:]).size > 0
-# end
-
-# def seed_db db, barbers #наполняется из №47, далее итерация; and #саll №13
-# 	barbers.each do |barber|
-# 		#проверка барбера сущ-ет или нет в таблице, если нет
-# 		if !is_barber_exists? db, barber
-# 			#то добавится в таблицу с помощью ниже команды
-# 			db.execute 'insert into Barbers (nameBarber) VALUES (?)', [barber]
-# 			end
-# 		end
-
-# end
 #вызывается каждый раз при перезагрузке сайт/страницы
-# before do
-# #инициализация БД
-# get_dbss
-
-# end
+before do
+#инициализация БД
+@barbers = Barber.all
+#Barber.order 'created_at desc'
+end
 
 get '/' do
-	@barbers = Barber.all
-			#Barber.order 'created_at desc'
 	erb :index
 end
+
 get '/visit' do
 @barberss = Barber.order(:name)
 erb :visit
 end
 
 post '/visit' do
-@barberss = Barber.order(:name)
-		@userName = params[:userName]
-		@userNumber = params[:userNumber]
-		@userDateTime = params[:userDateTime]
-		@barber = params[:barber]
-		@colorpicker = params[:colorpicker]
-	
-		hhErrorForms = {userName:'Не введено имя!',
-						userNumber: 'Не введено номер!',
-						userDateTime: 'Не введена дата и время'}
-
-	@error = hhErrorForms.select {|key,_| params[key] == ""}.values.join(", ")
-
-	if @error != ''
-		return erb :visit
-	end
-
-
+	@user = Client.new params[:client]
+	@user.save
+erb 'thank you your '
 end
 	
